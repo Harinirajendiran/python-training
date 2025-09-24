25b9d1488ae75265de90d85a3cfe7a8999abf6b3
Harinibme <rharini241102@gmail.com>
2025-09-24 11:21:29 +0530

    24-sep-2025
    24-sep-2025

---
+++ arrangeitems.py
@@ -0,0 +1,10 @@
+student=[
+    {"roll":1,"name":"kumar"},
+    {"roll":2,"name":"ganesh"},
+    {"roll":3,"name":"raj"}
+    ]
+print("Name    Rollnr")
+for i in student:
+    print(i["roll"],'\t',i["name"])
+    
+      
